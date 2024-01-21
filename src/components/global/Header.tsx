ca90ca09509933b33393b3d4b42b01ee14cd60b9
muhammadfathurraiyan <raiyanfathur@gmail.com>
2024-01-21 22:27:59 +0700

    update

--- src/components/global/Header.tsx
+++ src/components/global/Header.tsx
@@ -1,14 +1,14 @@
 import Link from "next/link";
-import { FaMagnifyingGlass } from "react-icons/fa6";
+import { FaBars, FaMagnifyingGlass } from "react-icons/fa6";
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center pt-4 pb-1">
+    <header className="flex justify-between items-center pt-4 pb-1 max-lg:px-4">
       <nav className="flex items-center gap-4">
         <Link href="/" className="text-xl font-bold uppercase">
-          MindBlogging
+          exception
         </Link>
-        <div className="flex gap-2  pl-4 border-l">
+        <div className="flex max-md:hidden gap-2 pl-4 border-l">
           <Link href="/conspiracy" className="text-sm uppercase">
             Conspiracy
           </Link>
@@ -20,7 +20,7 @@ export default function Header() {
           </Link>
         </div>
       </nav>
-      <form action="" className="flex items-center">
+      <form action="" className="flex max-md:hidden items-center">
         <input
           type="text"
           className="outline-none text-sm py-1 px-2 font-light border border-slate-900 bg-slate-900"
@@ -30,6 +30,9 @@ export default function Header() {
           <FaMagnifyingGlass />
         </button>
       </form>
+      <button className="py-2 md:hidden px-4 bg-slate-900/50 text-sm">
+        <FaBars />
+      </button>
     </header>
   );
 }
